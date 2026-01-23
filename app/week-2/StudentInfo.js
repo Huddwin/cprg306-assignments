ca78fd3dd15f34a4299c57be7d4937145e652bfe
Huddwin <huddwin.almeida@edu.sait.ca>
2026-01-23 15:00:16 -0700

    week-2

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Huddwin Almeida</p>
+            <Link href="https://github.com/huddwin/cprg306-assignments">
+            https://github.com/huddwin/cprg306-assignments</Link>
+        </div>
+    )
+}
