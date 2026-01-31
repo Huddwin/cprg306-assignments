01432750186200de07013a810521af64fa92ec5c
Huddwin <huddwin.almeida@edu.sait.ca>
2026-01-30 22:58:56 -0700

    week-3

---
+++ app/week-3/GroceryItem.js
@@ -0,0 +1,9 @@
+export default function GroceryItem({name, quantity, category}){
+    return (
+        <li className="bg-white p-2 m-2 border rounded">
+            <p className="text-blue-800 font-medium">{name}</p>
+            <p className="text-sm text-blue-800">Quantity: {quantity}</p>
+            <p className="text-sm text-blue-800 capitalize">Category: {category}</p>
+        </li>
+    );
+}
